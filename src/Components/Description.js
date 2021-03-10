db99e30a2ed38051217afc28918c4fba105a0baa
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-10 18:57:12 -0500

    created Description.js

---
+++ src/Components/Description.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './description.css';
+
+export default class Description extends Component {
+    render() {
+        return (
+            <div>
+                <p className="description">&emsp;Find a dish that goes will with a given wine (ex. Malbec, Pinot Grigio, Rose Wine, ... etc.). Also you can find a wine that goes well with a food. Food can be a dish name ("steak"), an ingredient name ("salmon"), or a cuisine ("itailan").</p>
+            </div>
+        )
+    }
+}
