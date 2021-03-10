f8c6f727c14873cc1537d3a7946ab3688e1a4c5c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-10 16:56:50 -0500

    working on Wine App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Title from './Components/Title';
 import { Search } from './Components/Search';
+import { SearchFood } from './Components/SearchFood';
+import { Button } from './Components/Button';
 
-function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      check: true
+    };
+  }
+
+  handleChangeDishPairingForWine() {
+    this.setState({
+      check: true
+    });
+  }
+
+  handleChangeWinePairing() {
+    this.setState({
+      check: false
+    });
+  }
+
+  render() {
   return (
     <div>
       <Title />
-      <Search />
+      {this.state.check ? <Search /> : <SearchFood />}
     </div>
   );
 }
+}
 
-export default App;
+// export default App;
