9c4f68d3aed782448c9e4220a94562143f32ba32
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-11 17:32:32 -0500

    working on project

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -5,7 +5,7 @@ export default class Description extends Component {
     render() {
         return (
             <div>
-                <p className="description">&emsp;Find a dish that goes well with a given wine (ex. Malbec, Pinot Grigio, Rose Wine, ... etc.). Also you can find a wine that goes well with a food. Food can be a dish name ("steak"), an ingredient name ("salmon"), or a cuisine ("Itailan").</p>
+                <p className="description">&emsp;Find a dish that goes well with a given wine (ex. Malbec, Pinot Grigio, Rose Wine, ... etc.). Also you can find a wine that goes well with a food. Food can be a dish name ("steak"), an ingredient name ("salmon"), or a cuisine ("Italian").</p>
             </div>
         )
     }
