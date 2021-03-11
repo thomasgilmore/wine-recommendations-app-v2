52d935be5cc2e479381093f6264d9fe392261b2f
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-10 20:41:47 -0500

    created Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="icons8">
+                Wine Recommendations App by <a target="_blank" href="https://github.com/thomasgilmore95">Thomas.</a> <a target="_blank" href="https://icons8.com/icons/set/wine-glass--v2">Wine Glass icon</a> by <a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+        )
+    }
+}
