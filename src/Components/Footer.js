fbaf4247ae34383e3c75eccbd9bbe3102da0058f
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-10 20:44:32 -0500

    working on Wine Recommendations App

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="icons8">
-                Wine Recommendations App by <a target="_blank" href="https://github.com/thomasgilmore95">Thomas.</a> <a target="_blank" href="https://icons8.com/icons/set/wine-glass--v2">Wine Glass icon</a> by <a target="_blank" href="https://icons8.com">Icons8</a>
+                Wine Recommendations App by <a target="_blank" href="https://github.com/thomasgilmore95">Thomas</a>. <a target="_blank" href="https://icons8.com/icons/set/wine-glass--v2">Wine Glass icon</a> by <a target="_blank" href="https://icons8.com">Icons8</a>
             </div>
         )
     }
