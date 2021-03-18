a03ea0d0dc90a2cedecdd78227d35eba5691271f
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-18 18:55:33 -0400

    SearchField.js

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default function SearchField() {
-    const searchDatabase = (e) => {
+    const searchWineAPI = (e) => {
         e.preventDefault();
         let inputValue = document.getElementById('searchField').value;
-        console.log(inputValue);
+        // console.log(inputValue);
     };
     return (
         <div>
             <form id="form">
-                <input type="text" placeholder="Search Movies or TV Shows..." id="searchField"></input>
-                <button type="submit" onClick={searchDatabase}>Search</button>
+                <input type="text" placeholder="Search Wine or Food..." id="searchField"></input>
+                <button type="submit" onClick={searchWineAPI}>Search</button>
             </form>
         </div>
     )
