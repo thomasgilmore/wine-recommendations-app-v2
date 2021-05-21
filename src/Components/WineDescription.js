a5eff490b5f2438b8f608a267ca4ac0c8314c711
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-21 18:59:24 -0400

    WineDescription

---
+++ src/Components/WineDescription.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './description.css';
+
+export default class WineDescription extends Component {
+    render() {
+        return (
+            <div>
+                <p className="description">&emsp;Find a dish that goes well with a given wine (ex. Malbec, Pinot Grigio, Rose Wine, ... etc.).</p>
+            </div>
+        )
+    }
+}
