c059985b417404a2620dde1bcb706100f5f9ed42
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-10 20:06:54 -0400

    rel=noreferrer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                Wine Recommendations App by <a target="_blank" href="https://github.com/thomasgilmore95" rel="noreferrer">Thomas</a>. API used for Wine Recommendations App by <a target="_blank" href="https://spoonacular.com/food-api" rel="noreferrer">Spoonacular</a>. Main picture from <a target="_blank" href="https://unsplash.com/">Unsplash</a>. Food and Wine Pictures from <a target="_blank" href="https://pixabay.com/sk/service/about/api/">Pixabay</a>. <a target="_blank" href="https://icons8.com/icons/set/wine-glass--v2" rel="noreferrer">Wine Glass icon</a> by <a target="_blank" href="https://icons8.com" rel="noreferrer">Icons8</a>
+                Wine Recommendations App by <a target="_blank" href="https://github.com/thomasgilmore95" rel="noreferrer">Thomas</a>. API used for Wine Recommendations App by <a target="_blank" href="https://spoonacular.com/food-api" rel="noreferrer">Spoonacular</a>. Main picture from <a target="_blank" href="https://unsplash.com/" rel="noreferrer">Unsplash</a>. Food and Wine Pictures from <a target="_blank" href="https://pixabay.com/sk/service/about/api/" rel="noreferrer">Pixabay</a>. <a target="_blank" href="https://icons8.com/icons/set/wine-glass--v2" rel="noreferrer">Wine Glass icon</a> by <a target="_blank" href="https://icons8.com" rel="noreferrer">Icons8</a>
             </div>
         )
     }
